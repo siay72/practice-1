566ecf8c39f689686feb454f1f5a3d8378700555
DevloperSiam <mdsiamh77@gmail.com>
2025-06-16 04:37:37 +0400

    4th and 5th ques

---
+++ dividedNum.js
@@ -0,0 +1,6 @@
+for (let i = 1; i <= 50; i++) {
+    const numbers = i;
+  if (numbers % 3 === 0 && numbers % 5 === 0) {
+    console.log(numbers);
+  }
+}
---
+++ leapYear.js
@@ -0,0 +1,10 @@
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isLip = isLeapYear(2052);
+console.log(isLip);
