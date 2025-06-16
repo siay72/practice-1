03de8bef7e296667ee55ed844a8979041a5324d0
DevloperSiam <mdsiamh77@gmail.com>
2025-06-16 04:02:06 +0400

    2nd question

---
+++ oddOrEven.js
@@ -0,0 +1,10 @@
+const oddOrEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, "It's Even Number");
+  } else {
+    console.log(number,  "It's Odd Number");
+  }
+};
+
+oddOrEven(7); 
+oddOrEven(10);
